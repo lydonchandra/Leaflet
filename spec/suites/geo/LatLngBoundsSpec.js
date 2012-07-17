efeb4dd07383842ba7fc75f6e7824c2dcee24c56
don <don>
2012-07-17 23:11:43 +0800

    added new tests for LatLngBounds

--- spec/suites/geo/LatLngBoundsSpec.js
+++ spec/suites/geo/LatLngBoundsSpec.js
@@ -1,5 +1,5 @@
 describe('LatLngBounds', function(){
-    describe('', function() {
+    describe('#contains', function() {
         it('should contain bound2', function() {
             var bound1 = new L.LatLngBounds( new L.LatLng(0,0),
                                              new L.LatLng(90,90) );
@@ -8,8 +8,6 @@ describe('LatLngBounds', function(){
                                              new L.LatLng(89,89) );
 
             expect( bound1.contains(bound2)).toBe(true);
-
-
 
             bound1 = new L.LatLngBounds( new L.LatLng(0,0),
                                          new L.LatLng(90,90));
@@ -30,8 +28,96 @@ describe('LatLngBounds', function(){
             expect( bound1.contains(bound2)).toBe(false);
             expect( bound2.contains(bound1)).toBe(true);
 
+            bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                new L.LatLng(1,1));
 
-
+            bound2 = new L.LatLngBounds( new L.LatLng(5,5),
+                new L.LatLng(10,10));
+            expect( bound1.contains( bound2)).toBe(false);
+            expect( bound2.contains(bound1)).toBe(false);
         });
-    })
+    });
+
+    describe('#equals', function() {
+        it('should be equal', function() {
+
+            var bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                                             new L.LatLng(90, 90));
+
+            var bound2 = new L.LatLngBounds( new L.LatLng(0,0 ),
+                                             new L.LatLng(90,90));
+            expect( bound1.equals( bound2)).toBe(true);
+            expect( bound2.equals(bound1)).toBe(true);
+        });
+
+        it('should not be equal', function() {
+
+            var bound1 = new L.LatLngBounds( new L.LatLng(1,1),
+                new L.LatLng(91, 91));
+
+            var bound2 = new L.LatLngBounds( new L.LatLng(0,0 ),
+                new L.LatLng(90,90));
+            expect( bound1.equals( bound2)).toBe(false);
+            expect( bound2.equals(bound1)).toBe(false);
+        });
+    });
+
+    describe('#intersects', function() {
+        it('should intersect each other', function() {
+            var bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                new L.LatLng(90, 90));
+
+            var bound2 = new L.LatLngBounds( new L.LatLng(0,0 ),
+                new L.LatLng(90,90));
+            expect( bound1.intersects(bound2)).toBe(true);
+            expect( bound2.intersects(bound1)).toBe(true);
+
+            bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                                         new L.LatLng(10,10));
+
+            bound2 = new L.LatLngBounds( new L.LatLng(10,10),
+                                         new L.LatLng(15,15));
+            expect( bound1.intersects(bound2)).toBe(true);
+            expect( bound2.intersects(bound1)).toBe(true);
+
+
+            bound2 = new L.LatLngBounds( new L.LatLng(5, 5),
+                                         new L.LatLng(11,11));
+            expect(bound1.intersects(bound2)).toBe(true);
+            expect(bound2.intersects(bound1)).toBe(true);
+        });
+
+        it('should not intersect', function() {
+            var bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                new L.LatLng(1, 1));
+
+            var bound2 = new L.LatLngBounds( new L.LatLng(2,2 ),
+                new L.LatLng(3,3));
+            expect( bound1.intersects(bound2)).toBe(false);
+            expect( bound2.intersects(bound1)).toBe(false);
+        });
+    });
+
+    describe('#extend', function() {
+        it('should extend 2nd bound', function() {
+            var bound1 = new L.LatLngBounds( new L.LatLng(0,0),
+                                             new L.LatLng(1,1));
+
+            var bound2 = new L.LatLngBounds( new L.LatLng(0,0),
+                                             new L.LatLng(5,5));
+            bound1.extend(bound2);
+            var extendedBounds = new L.LatLngBounds( new L.LatLng(0,0),
+                                                     new L.LatLng(5,5));
+            expect(bound1.equals(extendedBounds)).toBe(true);
+
+
+            bound2 = new L.LatLngBounds( new L.LatLng(-10,-10),
+                                         new L.LatLng(10,10));
+            extendedBounds = new L.LatLngBounds( new L.LatLng(-10,-10),
+                                                 new L.LatLng(10,10));
+            bound1.extend(bound2);
+            expect(bound1.equals(extendedBounds)).toBe(true);
+            expect(bound1.equals(bound2)).toBe(true);
+        });
+    });
 });
